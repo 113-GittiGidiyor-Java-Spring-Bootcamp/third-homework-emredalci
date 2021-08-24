100f7e4f74d52af641757aaee69789a7abcc8c61
Emre <emredalc@gmail.com>
2021-08-24 20:32:52 +0300

    instructor service updated

--- third-homework/src/main/java/dev/patika/thirdhomework/service/InstructorService.java
+++ third-homework/src/main/java/dev/patika/thirdhomework/service/InstructorService.java
@@ -4,7 +4,9 @@ import dev.patika.thirdhomework.entity.Instructor;
 import dev.patika.thirdhomework.repository.InstructorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,23 +16,30 @@ public class InstructorService implements BaseService<Instructor>{
     private final InstructorRepository instructorRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Instructor> findAll() {
-        return null;
+        List<Instructor> instructors = new ArrayList<>();
+        Iterable<Instructor> instructorIterable = instructorRepository.findAll();
+        instructorIterable.iterator().forEachRemaining(instructors::add);
+        return instructors;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Instructor findById(int id) {
-        return null;
+        return instructorRepository.findById(id).get();
     }
 
     @Override
-    public Instructor save(Instructor object) {
-        return null;
+    @Transactional
+    public Instructor save(Instructor instructor) {
+        return instructorRepository.save(instructor);
     }
 
     @Override
+    @Transactional
     public void deleteById(int id) {
-
+        instructorRepository.deleteById(id);
     }
 
     @Override
@@ -39,7 +48,7 @@ public class InstructorService implements BaseService<Instructor>{
     }
 
     @Override
-    public Instructor update(Instructor object) {
-        return null;
+    public Instructor update(Instructor instructor) {
+        return instructorRepository.save(instructor);
     }
 }
