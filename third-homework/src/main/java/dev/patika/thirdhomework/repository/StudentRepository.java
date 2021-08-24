fe2a0f117e0f232112804cbfaa186a006faafb48
Emre <emredalc@gmail.com>
2021-08-24 21:55:12 +0300

    gender statistics query added

--- third-homework/src/main/java/dev/patika/thirdhomework/repository/StudentRepository.java
+++ third-homework/src/main/java/dev/patika/thirdhomework/repository/StudentRepository.java
@@ -1,12 +1,18 @@
 package dev.patika.thirdhomework.repository;
 
 import dev.patika.thirdhomework.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student,Integer> {
 
     void deleteByName(String studentName);
+
+    @Query("select s.gender, count(s.gender) from Student s GROUP BY s.gender")
+    List<StudentGenderStatistics> getGendersWithGrouping();
 
 }
