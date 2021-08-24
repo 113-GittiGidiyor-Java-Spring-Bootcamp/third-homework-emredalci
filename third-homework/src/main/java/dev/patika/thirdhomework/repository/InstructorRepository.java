007911e3db8d09c91469e3457ea681dd20b36ea9
Emre <emredalc@gmail.com>
2021-08-24 22:34:30 +0300

    order by signatures added

--- third-homework/src/main/java/dev/patika/thirdhomework/repository/InstructorRepository.java
+++ third-homework/src/main/java/dev/patika/thirdhomework/repository/InstructorRepository.java
@@ -1,12 +1,19 @@
 package dev.patika.thirdhomework.repository;
 
 import dev.patika.thirdhomework.entity.Instructor;
+import dev.patika.thirdhomework.entity.PermanentInstructor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InstructorRepository extends CrudRepository<Instructor,Integer> {
 
     void deleteByName(String instructorName);
+
+    List<Instructor> findFirst3ByNameOrderBySalaryDesc();
+
+    List<Instructor> findFirst3ByNameOrderBySalaryAsc();
 
 }
